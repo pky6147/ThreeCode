6cab5dc1df11157443e6a9c649864564860e635c
crazyapoll0 <162910117+crazyapoll0@users.noreply.github.com>
2025-10-06 16:35:29 +0900

    백엔드.
    .

---
+++ TC_Back/src/main/java/com/tc_back/TcBackApplication.java
@@ -0,0 +1,13 @@
+package com.tc_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TcBackApplication.class, args);
+    }
+
+}
---
+++ TC_Back/src/test/java/com/tc_back/TcBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tc_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
