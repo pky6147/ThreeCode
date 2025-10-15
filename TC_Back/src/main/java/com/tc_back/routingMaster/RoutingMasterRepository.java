406bb678434da17aa896d849d6cb698b62e34d87
pky6147 <pky6147@naver.com>
2025-10-15 23:00:45 +0900

    라우팅정보
    .

--- TC_Back/src/main/java/com/tc_back/routingMaster/RoutingMasterRepository.java
+++ TC_Back/src/main/java/com/tc_back/routingMaster/RoutingMasterRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoutingMasterRepository extends JpaRepository<RoutingMaster,Long> {
     //중복코드
-    boolean existsByProcessCodeAndIsDelete(String processCode, String isDelete);
+    boolean existsByProcessCode(String processCode);
     boolean existsByProcessCodeAndIsDeleteAndRoutingMasterIdNot(String processCode, String isDelete, Long id);
 
 
