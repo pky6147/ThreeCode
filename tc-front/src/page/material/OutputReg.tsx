9bd9eb198308c905fe5dd5d8a0b3bf8cb491d0a8
ksm4949 <124114309+ksm4949@users.noreply.github.com>
2025-10-20 12:47:17 +0900

    원자재>출고등록 프론트수정

--- tc-front/src/page/material/OutputReg.tsx
+++ tc-front/src/page/material/OutputReg.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { Box, Typography, Card, TextField } from '@mui/material'
+import { Box, Typography, Card, TextField, Dialog } from '@mui/material'
 import CustomBC from '../../component/CustomBC';
 import CustomBtn from '../../component/CustomBtn';
 import CommonTable from '../../component/CommonTable';
@@ -8,38 +8,82 @@ import dayjs from 'dayjs'
 import { DatePicker } from '@mui/x-date-pickers/DatePicker'
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import LabelInput from '../../component/LabelInput';
+import SearchBar from '../../component/SearchBar';
+import ExcelBtn from '../../component/ExcelBtn';
+import type { AxiosError } from 'axios';
+import AlertPopup, { type AlertProps } from '../../component/AlertPopup';
 
 interface RowData {
     id: number;
-    material_input_no: string;
-    company_name: string;
-    material_no: string;
-    material_name: string;
+    materialInputId?: number;
+    materialInputNo: string;
+    companyName: string;
+    materialNo: string;
+    materialName: string;
     maker: string;
-    // material_input_qty: number | string;
-    // material_input_date: number | string;
-    stock_qty?: number | string;
-    material_output_qty?: number | string;
-    material_output_date?: number | string;
+    stockQty?: number;
+    materialOutputQty?: number;
+    materialOutputDate?: string;
 }
 
 function OutputReg() {
     const [rows, setRows ] = useState<RowData[]>([])
+    /* Search */
+    const [searchInfo, setSearchInfo] = useState({
+            companyName: '',
+            materialNo: '',
+            materialName: '',
+            materialInputNo: '',
+        })
+    const [searchRows, setSearchRows] = useState<RowData[]>([])
+    const [isSearch, setIsSearch] = useState(false)
+    /* Alert */
+    const [alertOpen, setAlertOpen] = useState(false)
+    const [alertInfo, setAlertInfo] = useState<AlertProps>({})
+
+    const getMaterialInputData = async () => {
+        try {
+            // const inputData = await getMaterialInput();
+            
+            // const result = inputData
+            // .map((row:RowData, index:number) => ({
+            //     ...row,
+            //     id: row.materialInputId,
+            //     idx: index+1,
+            //     materialInputQty: 0,
+            //     materialInputDate: '',
+            //     makeDate: ''
+            // }))
+
+            // setRows(result)
+        }
+        catch(err) {
+            console.error(err)
+            alert("조회 실패!")
+        }
+    };
+    const BoardRefresh = () => {
+        getMaterialInputData();
+    }
 
     useEffect(()=> {
-            const baseRow = [
-                { id: 1, material_input_no: 'MINC-20251015-001', company_name: '업체A', material_no: 'P001', material_name: '원자재1', maker: '제조사A' },
-                { id: 2, material_input_no: 'MINC-20251015-002', company_name: '업체B', material_no: 'P002', material_name: '원자재2', maker: '제조사B' },
-                { id: 3, material_input_no: 'MINC-20251015-003', company_name: '업체1', material_no: 'P010', material_name: '원자재3', maker: '제조사C' },
-                { id: 4, material_input_no: 'MINC-20251015-004', company_name: '업체2', material_no: 'P100', material_name: '원자재4', maker: '제조사D' },
-            ]
-    
-            const result = baseRow.map(r => ({
-                ...r, 
-                stock_qty: 500 // DB에서 계산해서 갖고올것
-            }))
-            setRows(result)
-    
+        getMaterialInputData()
+        const baseRow = [
+            { materialInputId: 1, materialInputNo: 'MINC-20251015-001', companyName: '업체A', materialNo: 'P001', materialName: '원자재1', maker: '제조사A', stockQty: 5000 },
+            { materialInputId: 2, materialInputNo: 'MINC-20251015-002', companyName: '업체B', materialNo: 'P002', materialName: '원자재2', maker: '제조사B', stockQty: 5000 },
+            { materialInputId: 3, materialInputNo: 'MINC-20251015-003', companyName: '업체1', materialNo: 'P010', materialName: '원자재3', maker: '제조사C', stockQty: 5000 },
+            { materialInputId: 4, materialInputNo: 'MINC-20251015-004', companyName: '업체2', materialNo: 'P100', materialName: '원자재4', maker: '제조사D', stockQty: 5000 },
+        ]
+
+        const result = baseRow.map(r => ({
+            ...r,
+            id: r.materialInputId, 
+            stock_qty: 500, // DB에서 계산해서 갖고올것
+            materialOutputQty: 0,
+            materialOutputDate: ''
+        }))
+        setRows(result)
     }, [])
 
     const handleChange = <K extends keyof RowData> (
@@ -54,47 +98,144 @@ function OutputReg() {
         )
     }
 
-    const handleOutput = (row: RowData) => {
+    // 출고
+    const handleOutput = async (row: RowData) => {
         console.log('row값', row)
+        
+        try {
+            // await createMaterialInput({
+            //     materialOutputQty: row.materialOutputQty,
+            //     materialOutputDate: row.materialOutputDate,
+            // })
+            handleAlertSuccess()
+            BoardRefresh()
+        } catch(err) {
+            const axiosError = err as AxiosError;
+            console.error(err)
+            if (axiosError.response && axiosError.response.data) {
+                handleAlertFail()
+                // alert((axiosError.response.data as AxiosError).message || "저장을 실패했습니다.");
+            } else {
+                handleAlertFail()
+            }
+        }
     }
 
+    /* Search */
+    const handleSearch = () => {
+        setIsSearch(true)
+        const filtered = rows.filter(row =>
+            (row.companyName?.toLowerCase() || '').includes(searchInfo.companyName.toLowerCase()) &&
+            (row.materialNo?.toLowerCase() || '').includes(searchInfo.materialNo.toLowerCase()) &&
+            (row.materialName?.toLowerCase() || '').includes(searchInfo.materialName.toLowerCase()) &&
+            (row.materialInputNo?.toLowerCase() || '').includes(searchInfo.materialInputNo.toLowerCase()) 
+        )
+        setSearchRows(filtered)
+    }
+    const handleReset = () => {
+        setIsSearch(false)
+        setSearchInfo({
+            companyName: '',
+            materialNo: '',
+            materialName: '',
+            materialInputNo: '',
+        })
+        setSearchRows(rows)
+    }
+    const handleSearchChange = (key: keyof typeof searchInfo, value: string) => {
+        setSearchInfo((prev) => ({ ...prev, [key]: value }));
+    };
+
+    /* Alert 팝업 */
+    const handleCloseAlert = () => {
+        setAlertOpen(false)
+    }
+    const handleAlertSuccess = () => {
+        setAlertInfo({
+            type: 'success',
+            title: '원자재 출고 등록',
+            text: '원자재 출고 등록이 완료되었습니다.'
+        })
+        setAlertOpen(true)
+
+        setTimeout(() => setAlertOpen(false), 2000)
+    }
+    const handleAlertFail = () => {
+        setAlertInfo({
+            type: 'error',
+            title: '원자재 출고 등록',
+            text: '원자재 출고 등록 실패'
+        })
+        setAlertOpen(true)
+
+        setTimeout(()=> setAlertOpen(false), 3000)
+    }
+    /* ExcelBtn Props */
+    interface ExcelData {
+        materialInputNo: string;
+        companyName: string;
+        materialNo: string;
+        materialName: string;
+        maker: string;
+        stockQty: number;
+    }
+    // 엑셀 컬럼 헤더 매핑 정의
+    const headerMap: Record<keyof ExcelData, string> = {
+        materialInputNo: '입고번호',
+        companyName: '매입처명',
+        materialNo: '품목번호',
+        materialName: '품목명',
+        maker: '제조사',
+        stockQty: '재고량'
+    }
+    const excelData = rows.map(row =>
+        (Object.keys(headerMap) as (keyof ExcelData)[]).reduce<Record<string, string>> (
+            (acc, key) => {
+                const value = row[key];
+                acc[headerMap[key]] = value != null ? String(value) : '';
+                return acc;
+            },
+            {}
+        )
+    );
+
     const columns: GridColDef[] = [
-        { field: 'material_input_no', headerName: '입고번호', width: 150, headerAlign: 'center', align: 'center' },
-        { field: 'company_name', headerName: '매입처명', flex: 1.5, minWidth: 150, headerAlign: 'center', align: 'center' },
-        { field: 'material_no', headerName: '품목번호', flex: 1, minWidth: 100, headerAlign: 'center', align: 'center' },
-        { field: 'material_name', headerName: '품목명', flex: 1.5, minWidth: 150, headerAlign: 'center', align: 'center' },
+        { field: 'materialInputNo', headerName: '입고번호', width: 180, headerAlign: 'center', align: 'center' },
+        { field: 'companyName', headerName: '매입처명', flex: 1.5, minWidth: 150, headerAlign: 'center', align: 'center' },
+        { field: 'materialNo', headerName: '품목번호', flex: 1, minWidth: 100, headerAlign: 'center', align: 'center' },
+        { field: 'materialName', headerName: '품목명', flex: 1.5, minWidth: 150, headerAlign: 'center', align: 'center' },
         { field: 'maker', headerName: '제조사', flex: 1, minWidth: 100, headerAlign: 'center', align: 'center' },
-        { field: 'stock_qty', headerName: '재고량', flex: 1, minWidth: 100, headerAlign: 'center', align: 'right',
+        { field: 'stockQty', headerName: '재고량', flex: 1, minWidth: 100, headerAlign: 'center', align: 'right',
             renderCell: (params) => {
                 const value = params.value;
                 return value?.toLocaleString();  // 천단위 콤마
             }
         },
-        { field: 'material_output_qty', headerName: '출고수량', flex: 1.5, minWidth: 150, headerAlign: 'center', align: 'right',
+        { field: 'materialOutputQty', headerName: '출고수량', flex: 1.5, minWidth: 150, headerAlign: 'center', align: 'right',
             renderCell: (params) => (
                 <TextField
                     type="text"
                     size="small"
-                    value={params.row.material_output_qty?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") || ''}
+                    value={params.row.materialOutputQty?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") || ''}
                     onChange={(e) => {
                         // 입력값에서 콤마 제거 후 숫자로 변환
                         const rawValue = e.target.value.replace(/,/g, '');
-                        handleChange(params.row.id, 'material_output_qty', rawValue)
+                        handleChange(params.row.id, 'materialOutputQty', Number(rawValue))
                     }}
                     sx={{ width: '100%', paddingTop: 0.7, display: 'flex' }}
                     InputProps={{ sx: { '& input': { textAlign: 'right' } } }}
                 />
             )
         },
-        { field: 'material_output_date', headerName: '출고일자', flex: 2, minWidth: 200, headerAlign: 'center', align: 'center',
+        { field: 'materialOutputDate', headerName: '출고일자', flex: 2, minWidth: 200, headerAlign: 'center', align: 'center',
             renderCell: (params) => (
                 <DatePicker
                   format="YYYY-MM-DD"
-                  value={params.row.material_output_date ? dayjs(params.row.material_output_date) : null}
+                  value={params.row.materialOutputDate ? dayjs(params.row.materialOutputDate) : null}
                   onChange={(newValue) =>
                     handleChange(
                       params.row.id,
-                      'material_output_date',
+                      'materialOutputDate',
                       newValue?.format('YYYY-MM-DD') || ''
                     )
                   }
@@ -112,6 +253,7 @@ function OutputReg() {
                 <CustomBtn
                   width="50px"
                   text="출고"
+                  icon="check"
                   onClick={() => handleOutput(params.row)}
                 >
                 </CustomBtn>
@@ -129,30 +271,63 @@ function OutputReg() {
                     {/* Breadcrumbs 영역 */}
                     <CustomBC text="출고 등록" subText='원자재 입출고 관리' />
                     {/* Content 영역 */}
+                    <Box sx={{padding: 2}}>
+                        <SearchBar onSearch={handleSearch} onReset={handleReset}>
+                            <LabelInput 
+                                labelText='매입처명'
+                                value={searchInfo.companyName}
+                                onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleSearchChange('companyName', e.target.value)}
+                            />
+                            <LabelInput 
+                                labelText='품목번호'
+                                value={searchInfo.materialNo}
+                                onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleSearchChange('materialNo', e.target.value)}
+                            />
+                            <LabelInput 
+                                labelText='품목명'
+                                value={searchInfo.materialName}
+                                onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleSearchChange('materialName', e.target.value)}
+                            />
+                            <LabelInput 
+                                labelText='입고번호'
+                                value={searchInfo.materialInputNo}
+                                onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleSearchChange('materialInputNo', e.target.value)}
+                            />
+                        </SearchBar>
+                    </Box>
                     <Box>
                         <Box
                             sx={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}
                         >
                             <Typography sx={{ fontSize: '24px', fontWeight: 'bold', paddingLeft: 2 }}>원자재 품목 출고 등록</Typography>
                             <Box sx={{paddingRight: 2}}>
-                                <CustomBtn 
-                                    text="엑셀"
-                                    backgroundColor='green'
-                                />
+                                <ExcelBtn mappingdata={excelData} sheetName="원자재 출고 등록" fileName="원자재 출고 등록" />
                             </Box>
                         </Box>
 
                         <Box sx={{padding: 2}}>
+                            { isSearch ? (
+                            <CommonTable 
+                            columns={columns}
+                            rows={searchRows}
+                            />
+                            ) : (
                             <CommonTable 
                                 columns={columns}
                                 rows={rows}
-                                // pageSize={10}
-                                // check={true}
-                                // height={630}
                             />
+                            )}
                         </Box>
                     </Box>
                 </Box>
+                {/* 팝업창 */}
+                <Dialog open={alertOpen} onClose={handleCloseAlert}>
+                    <AlertPopup 
+                        type={alertInfo.type} 
+                        title={alertInfo.title} 
+                        text={alertInfo.text} 
+                    />
+                </Dialog>
             </Card>
         </LocalizationProvider>
     )
