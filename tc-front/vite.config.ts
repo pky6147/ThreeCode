691401e85edcede278ddf86997796f0ff2e21d07
pky6147 <pky6147@naver.com>
2025-10-23 17:11:43 +0900

    서버변경
    .

--- tc-front/vite.config.ts
+++ tc-front/vite.config.ts
@@ -1,22 +1,49 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     host: "localhost",
+//     proxy: {
+//       '/api': {
+//         target: "http://localhost:8080",
+//         rewrite:(path) => path.replace(/^\/api/,""),
+//         changeOrigin: true
+//       },
+//       '/images': {               // ✅ 이미지 요청도 프록시
+//         target: "http://localhost:8080",
+//         changeOrigin: true
+//       }
+//     }
+//   }
+// })
+
+
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: "localhost",
+   server: {
+    host: "0.0.0.0",
+    port: 80,
+    strictPort: true,
     proxy: {
       '/api': {
-        target: "http://localhost:8080",
-        rewrite:(path) => path.replace(/^\/api/,""),
+        target: 'http://172.20.20.4:8080',
+        rewrite: (path) => path.replace(/^\/api/, ""),
         changeOrigin: true
       },
       '/images': {               // ✅ 이미지 요청도 프록시
-        target: "http://localhost:8080",
+        target: "http://172.20.20.4:8080",
         changeOrigin: true
       }
     }
   }
-})
+  })
 
